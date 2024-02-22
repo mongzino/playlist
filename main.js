cb5d4727f87ff849f6a9dc5f737d6bcc4475f851
Jistan7011 <qus7011@gmail.com>
2024-02-22 09:29:23 +0900

    js to make branch =

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
 import { MongoClient } from "mongodb";
 import getInput from "userInput";
+import getUserInput from "./userInput";
 
 const uri = process.env.DB_URL;
 const client = new MongoClient(uri);
 
-function main() {}
+async function main() {
+    while(true){
+        console.log("치타 뮤직에 오신 것을 환영합니다")
+        console.log("1.회원 가입   2.로그인   3.프로그램 종료")
+        let mainInput=await getUserInput()
+        console.log(mainInput)
+    }
+}
 
 main();
 
