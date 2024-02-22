878f8660929f4937985fae2b2345096b93fa259e
lahyeok <thisisdbdb1@gmail.com>
2024-02-22 22:23:06 +0900

    run error fix

--- myPage.js
+++ myPage.js
@@ -1,10 +1,10 @@
-import { MongoClient } from 'mongodb';
-import { getUserInput } from "./userInput";
+import { MongoClient } from "mongodb";
+import getUserInput from "./userInput.js";
 
 
 
 async function main(){
-    const uri = process.env.DB_ATLAS_URL;
+    const uri = process.env.DB_URL;
     const client = new MongoClient(uri);
 
     while(true){
@@ -17,28 +17,32 @@ async function main(){
             while(true) {
                 console.log('내정보 관리 - 1.회원 탈퇴 2.프로필 뮤직 설정 3.선호 장르 설정 4.뒤로가기');
                 let user = await getUserInput();
-                if(menu==='1'){
+                if(user==='1'){
                     console.log(`회원 탈퇴 하시겠습니까`)
-                }else if(menu==='2'){
+                }else if(user==='2'){
                     console.log(`프로필 뮤직 설정 화면입니다`)
-                }else if(menu==='3'){
+                }else if(user==='3'){
                     console.log(`선호 장르 설정 화면입니다`)
-                }else if(menu==='4'){
+                }else if(user==='4'){
                     console.log(`뒤로가기`)
                     await wait(500);
-                    process.exit();
+                    console.clear();
+                    break;
                 }else{
                     console.log('잘못된 입력입니다.');
-                    return;
+                    await wait(500);
+                    console.clear();
                 }
             }
         }else if(menu==='3'){
             console.log(`뒤로가기`);
             await wait(500);
-            process.exit();
+            console.clear();
+            break;
         }else{
             console.log('잘못된 입력입니다.');
-            return;
+            await wait(500);
+            console.clear();
         }
     }
 }
