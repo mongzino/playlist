c105d935459d662c18c12e6c6a96dc9eb14d0467
Oh-seo-jin <seojin2414@gmail.com>
2024-02-23 10:51:36 +0900

    applied myPlaylist to myPage

--- myPage.js
+++ myPage.js
@@ -1,52 +1,94 @@
 import { MongoClient } from "mongodb";
 import getUserInput from "./userInput.js";
+import { create, find, modify, remove } from "./myPlaylist.js";
 
+async function main(client, user_id) {
+  while (true) {
+    console.log(`마이페이지 - 1.플레이리스트 관리 2.내 정보 관리 3.뒤로가기`);
+    let menu = await getUserInput();
+    // 플레이리스트 관리
+    if (menu === "1") {
+      // 사용자 정보 불러오기
+      const userInfo = await client
+        .db("PlaylistDB")
+        .collection("user")
+        .findOne({ _id: user_id });
 
-
-async function main(){
-    const uri = process.env.DB_URL;
-    const client = new MongoClient(uri);
-
-    while(true){
-        console.log(`마이페이지 - 1.플레이리스트 관리 2.내 정보 관리 3.뒤로가기`);
-        let menu = await getUserInput();
-        if(menu==='1') {
-            console.log('내 플레이리스트 관리하기>');
-            let title = await getUserInput();
-        }else if(menu==='2'){
-            while(true) {
-                console.log('내정보 관리 - 1.회원 탈퇴 2.프로필 뮤직 설정 3.선호 장르 설정 4.뒤로가기');
-                let user = await getUserInput();
-                if(user==='1'){
-                    console.log(`회원 탈퇴 하시겠습니까`)
-                }else if(user==='2'){
-                    console.log(`프로필 뮤직 설정 화면입니다`)
-                }else if(user==='3'){
-                    console.log(`선호 장르 설정 화면입니다`)
-                }else if(user==='4'){
-                    console.log(`뒤로가기`)
-                    await wait(500);
-                    console.clear();
-                    break;
-                }else{
-                    console.log('잘못된 입력입니다.');
-                    await wait(500);
-                    console.clear();
-                }
-            }
-        }else if(menu==='3'){
-            console.log(`뒤로가기`);
-            await wait(500);
-            console.clear();
-            break;
-        }else{
-            console.log('잘못된 입력입니다.');
-            await wait(500);
-            console.clear();
+      while (true) {
+        console.clear();
+        // 내 플레이리스트 목록 보여주기
+        const playlist = await client
+          .db("PlaylistDB")
+          .collection("playlist")
+          .find({ owner: user_id })
+          .toArray();
+        await playlist.forEach((pl) => {
+          pl.owner = userInfo.name;
+        });
+        console.table(playlist, ["owner", "title", "description", "views"]);
+        // 메뉴 선택
+        console.log(
+          "플레이리스트 관리 - 1: 플레이리스트 생성 2: 플레이리스트 조회 3: 플레이리스트 수정 4: 플레이리스트 삭제 5: 뒤로가기"
+        );
+        let pl_menu = parseInt(await getUserInput());
+        if (pl_menu === 1) {
+          // 플레이리스트 생성 함수
+          await create(client, user_id);
+        } else if (pl_menu === 2) {
+          // 선택한 플레이리스트의 수록곡 조회 함수
+          await find(client, playlist);
+        } else if (pl_menu === 3) {
+          // 선택한 플레이리스트 수정 함수
+          await modify(client, playlist);
+        } else if (pl_menu === 4) {
+          // 내 플레이리스트 목록 보여주는 함수
+          await remove(client, playlist);
+        } else if (pl_menu === 5) {
+          console.log("뒤로가기");
+          break;
+        } else {
+          console.log("잘못된 입력입니다. 메뉴를 다시 선택해주세요.");
         }
+      }
+    } else if (menu === "2") {
+      while (true) {
+        console.log(
+          "내정보 관리 - 1.회원 탈퇴 2.프로필 뮤직 설정 3.선호 장르 설정 4.뒤로가기"
+        );
+        let user = await getUserInput();
+        if (user === "1") {
+          console.log(`회원 탈퇴 하시겠습니까`);
+        } else if (user === "2") {
+          console.log(`프로필 뮤직 설정 화면입니다`);
+        } else if (user === "3") {
+          console.log(`선호 장르 설정 화면입니다`);
+        } else if (user === "4") {
+          console.log(`뒤로가기`);
+          await wait(500);
+          console.clear();
+          break;
+        } else {
+          console.log("잘못된 입력입니다.");
+          await wait(500);
+          console.clear();
+        }
+      }
+    } else if (menu === "3") {
+      console.log(`뒤로가기`);
+      await wait(500);
+      console.clear();
+      break;
+    } else {
+      console.log("잘못된 입력입니다.");
+      await wait(500);
+      console.clear();
     }
+  }
 }
 
-main();
+const wait = (timeToDelay) =>
+  new Promise((resolve) => setTimeout(resolve, timeToDelay));
 
-const wait = (timeToDelay) => new Promise((resolve) => setTimeout(resolve, timeToDelay));
+const uri = process.env.DB_URL;
+const client = new MongoClient(uri);
+// await main(client, 4000000);
--- myPlaylist.js
+++ myPlaylist.js
@@ -2,15 +2,15 @@ import { MongoClient } from "mongodb";
 import getInput from "./userInput.js";
 
 // Auto Increment
-async function getNextSequence(user_id) {
+async function getNextSequence(client, user_id) {
   var ret = await client
-  .db("PlaylistDB")
-  .collection("counters")
-  .findOneAndUpdate(
-    { _id: user_id },
-    { $inc: { seq: 1 } },
-    { returnDocument: 'after' }
-  );
+    .db("PlaylistDB")
+    .collection("counters")
+    .findOneAndUpdate(
+      { _id: user_id },
+      { $inc: { seq: 1 } },
+      { returnDocument: "after" }
+    );
   return ret.seq;
 }
 
@@ -21,14 +21,14 @@ export async function create(client, user_id) {
 
   console.log("♬ 생성할 플레이리스트의 설명을 입력해주세요");
   let description = await getInput();
-  
+
   try {
     await client
       .db("PlaylistDB")
       .collection("playlist")
       .insertOne({
         // 아이디 자동 부여하는 방법 추가
-        _id: await getNextSequence(user_id),
+        _id: await getNextSequence(client, user_id),
         music_id: [],
         owner: user_id,
         title: title,
@@ -52,17 +52,12 @@ export async function find(client, playlist) {
     console.log(":: DESCRIPTION:", playlist[sel].description);
     console.log(":: VIEWS:", playlist[sel].views);
     // 수록곡 출력
-    let musicList = [...playlist[sel].music_id]; // 배열 복사
-    for (let i = 0; i < musicList.length; i++) {
-      let musicInfo = await client
-        .db("PlaylistDB")
-        .collection("music")
-        .findOne({ _id: musicList[i] });
-      musicList[i] = musicInfo;
-    }
+    let musicList = await client
+      .db("PlaylistDB")
+      .collection("music")
+      .find({ playlist_id: playlist[sel]._id })
+      .toArray();
     console.table(musicList, ["title", "length", "composer", "lyricist"]);
-  } catch {
-    console.log("잘못된 입력입니다.");
   } finally {
     console.log(":: 조회를 끝내시려면 Enter를 입력해주세요");
     await getInput();
@@ -70,42 +65,48 @@ export async function find(client, playlist) {
 }
 
 export async function modify(client, playlist) {
-  console.log(
-    "%c♬ %c수정하고 싶은 플레이리스트 번호를 선택해주세요",
-    "color: green",
-    "color: white"
-  );
+  console.log("♬ 수정하고 싶은 플레이리스트 번호를 선택해주세요");
   const sel = parseInt(await getInput());
+  if (sel > playlist.length){
+    console.log("잘못된 입력입니다.")
+    await getInput()
+    return;
+  }
   console.clear();
-  console.log(
-    "%c♬ %c1: 수록곡 편집 %c2: 제목&설명 변경",
-    "color: green",
-    "color: yello",
-    "color: blue"
-  );
+  console.log("♬ 1: 수록곡 편집 2: 제목&설명 변경");
   const val = parseInt(await getInput());
   if (val === 1) {
     // 수록곡 출력
-    let musicList = [...playlist[sel].music_id]; // 배열 복사
-    for (let i = 0; i < musicList.length; i++) {
-      let musicInfo = await client
-        .db("PlaylistDB")
-        .collection("music")
-        .findOne({ _id: musicList[i] });
-      musicList[i] = musicInfo;
-    }
+    let musicList = await client
+      .db("PlaylistDB")
+      .collection("music")
+      .find({ playlist_id: playlist[sel]._id })
+      .toArray();
     console.table(musicList, ["title", "length", "composer", "lyricist"]);
-
     // 수록곡 편집
     console.log("♬ 삭제하고 싶은 수록곡의 번호를 입력해주세요");
     const music_sel = await getInput();
-    await client
-      .db("PlaylistDB")
-      .collection("playlist")
-      .updateOne(
-        { _id: playlist[sel]._id },
-        { $pull: { music_id: playlist[sel].music_id[music_sel] } } // $pull : 조건을 만족하는 요소 제거
+    try {
+      // playlist에서 music_id 삭제
+      await client
+        .db("PlaylistDB")
+        .collection("playlist")
+        .updateOne(
+          { _id: playlist[sel]._id },
+          { $pull: { music_id: playlist[sel].music_id[music_sel] } } // $pull : 조건을 만족하는 요소 제거
+        );
+      // music에서 playlist_id 삭제
+      await client.db("PlaylistDB")
+      .collection("music")
+      .updateOne({ _id: playlist[sel].music_id[music_sel] },
+        { $pull: { playlist_id: playlist[sel]._id } } // $pull : 조건을 만족하는 요소 제거
       );
+        console.log("수록곡이 삭제되었습니다.");
+    } catch {
+      console.log("잘못된 입력입니다.");
+    } finally {
+      await getInput();
+    }
   } else if (val === 2) {
     // 설정 변경
     console.log("♬ 새로운 플레이리스트 제목을 입력해주세요");
@@ -147,7 +148,7 @@ export async function remove(client, playlist) {
 
 // 파일 내 테스트용 main 함수
 async function main(client, user_id) {
-  let menu = 0;
+  let pl_menu = 0;
   // const db = "PlaylistDB";
   // const col = "playlist";
   // 사용자 정보 불러오기
@@ -156,7 +157,7 @@ async function main(client, user_id) {
     .collection("user")
     .findOne({ _id: user_id });
   // console.table(userInfo);
-  while (menu !== 5) {
+  while (pl_menu !== 5) {
     console.clear();
 
     // 내 플레이리스트 목록 보여주기
@@ -168,30 +169,30 @@ async function main(client, user_id) {
     playlist.forEach((pl) => {
       pl.owner = userInfo.name;
     });
-    console.table(playlist/*, ["owner", "title", "description", "views"]*/);
+    console.table(playlist /*, ["owner", "title", "description", "views"]*/);
 
     // 메뉴
     console.log(
       "1: 새로운 플레이리스트 만들기 2: 플레이리스트 조회하기 3: 플레이리스트 수정하기 4: 플레이리스트 삭제하기 5: 뒤로가기"
     );
-    menu = parseInt(await getInput());
-    if (menu === 1) {
+    pl_menu = parseInt(await getInput());
+    if (pl_menu === 1) {
       // console.log("새로운 플레이리스트 만들기");
       // 플레이리스트 생성 함수
       await create(client, user_id);
-    } else if (menu === 2) {
+    } else if (pl_menu === 2) {
       // console.log("플레이리스트 조회하기");
       // 선택한 플레이리스트의 수록곡 조회 함수
       await find(client, playlist);
-    } else if (menu === 3) {
+    } else if (pl_menu === 3) {
       // console.log("플레이리스트 수정하기");
       // 선택한 플레이리스트 수정 함수
       await modify(client, playlist);
-    } else if (menu === 4) {
+    } else if (pl_menu === 4) {
       // console.log("플레이리스트 삭제하기");
       // 내 플레이리스트 목록 보여주는 함수
       await remove(client, playlist);
-    } else if (menu === 5) {
+    } else if (pl_menu === 5) {
       console.log("%c뒤로가기", "color: yellow; font-style: italic;");
       process.exit();
       // return
@@ -201,8 +202,8 @@ async function main(client, user_id) {
   }
 }
 
-const uri = process.env.DB_URL;
-const client = new MongoClient(uri);
-client.connect();
-console.log("Connected successfully to server");
-main(client, 4000000);
+// const uri = process.env.DB_URL;
+// const client = new MongoClient(uri);
+// client.connect();
+// console.log("Connected successfully to server");
+// main(client, 4000000);
