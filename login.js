337d78475bc6c616420c22f115efacdb75a1441f
Oh-seo-jin <seojin2414@gmail.com>
2024-02-23 14:24:02 +0900

    add style

--- login.js
+++ login.js
@@ -1,11 +1,12 @@
 import getUserInput from "./userInput.js";
+import chalk from "chalk";
 
 export async function login(client) {
     while(true){
     // 사용자로부터 아이디와 비밀번호를 입력받습니다.
-    console.log("아이디를 입력하세요:");
+    console.log(chalk.bgWhiteBright("아이디를 입력하세요"));
     let id = await getUserInput();
-    console.log("비밀번호를 입력하세요:");
+    console.log(chalk.bgWhiteBright("비밀번호를 입력하세요"));
     let password = await getUserInput();
 
     // MongoDB에 로그인을 수행합니다.
@@ -17,11 +18,11 @@ export async function login(client) {
     const user = await collection.findOne(query);
 
     if (user) {
-        console.log(`로그인에 성공했습니다, ${user.name}님 환영합니다.`);
+        console.log(chalk.bgCyan(`로그인에 성공했습니다, ${user.name}님 환영합니다.`));
         return true
     } else {
         console.clear()
-        console.log("아이디 또는 비밀번호가 일치하지 않습니다.");
+        console.log(chalk.cyan("아이디 또는 비밀번호가 일치하지 않습니다."));
     }
 }
 }
--- main.js
+++ main.js
@@ -1,63 +1,68 @@
 import { MongoClient } from "mongodb";
-import getInput from "./userInput.js";
 import getUserInput from "./userInput.js";
-import ckName, { ckId,ckPassword,updateGenre,createAccount } from "./signUp.js";
-import {login} from "./login.js"
-
-
+import chalk from "chalk";
+import ckName, {
+  ckId,
+  ckPassword,
+  updateGenre,
+  createAccount,
+} from "./signUp.js";
+import { login } from "./login.js";
 
 const uri = process.env.DB_URL;
 const client = new MongoClient(uri);
 
 async function main() {
-    try{
-        while(true){
-            console.log("치타 뮤직에 오신 것을 환영합니다")
-            console.log("1.회원 가입   2.로그인   3.프로그램 종료")
-            let mainInput=await getUserInput()
-            
-            if(mainInput==1)
-            {
-                console.log("회원가입")
-                let name=await ckName(client)
-                let id=await ckId(client)
-                let password=await ckPassword(client)
-                if(name && id && password){
-                    
-                    console.log(`계정생성 되었습니다 환영합니다 ${name}님`)
-                    await createAccount(client,name,id,password)
-                    await updateGenre(client,id)
-                }
+  try {
+    while (true) {
+      console.log(
+        chalk.cyan(`
+██╗    ██╗███████╗██╗      ██████╗ ██████╗ ███╗   ███╗███████╗
+██║    ██║██╔════╝██║     ██╔════╝██╔═══██╗████╗ ████║██╔════╝
+██║ █╗ ██║█████╗  ██║     ██║     ██║   ██║██╔████╔██║█████╗
+██║███╗██║██╔══╝  ██║     ██║     ██║   ██║██║╚██╔╝██║██╔══╝
+╚███╔███╔╝███████╗███████╗╚██████╗╚██████╔╝██║ ╚═╝ ██║███████╗
+╚══╝╚══╝ ╚══════╝╚══════╝ ╚═════╝ ╚═════╝ ╚═╝     ╚═╝╚══════╝
+      `)
+      );
+      console.log(chalk.bgCyan("치타 뮤직에 오신 것을 환영합니다"));
+      console.log(chalk.white("1.회원 가입 2.로그인"), chalk.cyan("3.프로그램 종료"));
+      let mainInput = await getUserInput();
 
-
-            }
-            else if(mainInput==2)
-            {
-                console.log("로그인")
-                await login(client);
-                while(true){
-                console.log("1.검색하기 2.마이페이지 3.로그아웃")
-                let Umenu=await getUserInput()
-                if(Umenu==3)break
-                }
-            }
-            else if(mainInput==3)
-            {
-                console.log("프로그램을 종료합니다.")
-                process.exit()
-            }
-            else
-            {
-                console.log("해당하는 번호가 아닙니다.")
-            }
-
-            console.log("\n메인 메뉴로 넘어가려면 엔터를 입력하세요")
-            await getUserInput()
-            console.clear()
-            
+      if (mainInput == 1) {
+        let name = await ckName(client);
+        let id = await ckId(client);
+        let password = await ckPassword(client);
+        if (name && id && password) {
+          console.log(chalk.bgCyan(`\n계정생성 되었습니다 환영합니다 ${name}님`));
+          await createAccount(client, name, id, password);
+          await updateGenre(client, id);
         }
-    }catch(e){console.error}finally{await client.close()}
+      } else if (mainInput == 2) {
+        // console.log("로그인");
+        await login(client);
+        while (true) {
+          console.log(chalk.white("1.검색하기 2.마이페이지"), chalk.cyan("3.로그아웃"));
+          let Umenu = await getUserInput();
+          if (Umenu == 3) break;
+        }
+      } else if (mainInput == 3) {
+        console.log(chalk.cyan("프로그램을 종료합니다."));
+        process.exit();
+      } else {
+        console.log(chalk.cyan("해당하는 번호가 아닙니다."));
+      }
+
+      console.log(chalk.bgWhiteBright("\n메인 메뉴로 넘어가려면 엔터를 입력하세요"));
+      await getUserInput();
+      console.clear();
     }
+  } catch (e) {
+    console.error;
+  } finally {
+    await client.close();
+  }
+}
 
 main();
 
